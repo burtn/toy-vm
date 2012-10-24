7e5f2bf2dfadf87c66ed02f11eb39f37b4adfd30
Jake <jake5991@live.com>
2012-10-24 14:20:17 +0200

    Add break's to switch statement
    Fix issue with continuation of switch statement after block, causes STOP Fatal error during testing, should now be fixed. Ref: http://msdn.microsoft.com/en-us/library/66k51h7a(v=vs.80).aspx

--- runner.c
+++ runner.c
@@ -9,36 +9,48 @@ int run(void) {
 		switch (machine.instruction[machine.ip]) {
 			case SKP:
 				machine.ip++;
+                                    break;
 			case HLT:
 				return 0;
+                                    break;
 			case PRT:
 				print();
+                                    break;
 			case ADD:
 				add(machine.instruction[machine.ip++], machine.instruction[machine.ip +2]);
 				machine.ip = machine.ip + 2;
+                                    break;
 			case SUB:
 				sub(machine.instruction[machine.ip++], machine.instruction[machine.ip +2]);
 				machine.ip = machine.ip + 2;
+                                    break;
 			case MUL:
 				mul(machine.instruction[machine.ip++], machine.instruction[machine.ip +2]);
 				machine.ip = machine.ip + 2;
+                                    break;
 			case DIV:
 				div(machine.instruction[machine.ip++], machine.instruction[machine.ip +2]);
 				machine.ip = machine.ip + 2;
+                                    break;
 			case POP:
 				temp = pop();
+                                     break;
 			case PSH:
 				push(machine.instruction[machine.ip++]);
 				machine.ip++;
+                                    break;
 			case R2S:
 				register_to_stack(machine.instruction[machine.ip++]);
 				machine.ip++;
+                                    break;
 			case S2R:
 				stack_to_register(machine.instruction[machine.ip++]);
 				machine.ip++;
+                                    break;
 			case SET:
 				set(machine.instruction[machine.ip++], machine.instruction[machine.ip +2]);
 				machine.ip = machine.ip + 2;
+                                    break;
 			default:
 				return 1;
 		}
